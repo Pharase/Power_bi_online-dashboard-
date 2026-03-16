229b9261f0a544d7e9b0ba3cb2abda4a5d0e098e
Paramut Chanthakan <75171704+Pharase@users.noreply.github.com>
2026-03-16 10:52:44 +0700

    Update update.bat to use relative paths

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Activate conda environment and run the script
 
-CALL "C:\Users\PAMC-NB-Alpha\miniconda3\Scripts\activate.bat" activate base
-python "C:\pam_mis\BI_dashboard_data\bi_phonereport_update.py"
+CALL "User\miniconda3\Scripts\activate.bat" activate base
+python "bi_phonereport_update.py"
 pause
